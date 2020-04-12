9540014dd4d811929dee7e75e08a1bfbedc7bbe1
Erez Buchnik <erez@shrewdthings.com>
2020-04-12 17:26:55 +0300

    Add net blacklisting & distinct colors

--- netgraph/netgraph.py
+++ netgraph/netgraph.py
@@ -11,8 +11,57 @@ from graphviz import Graph
 from graphviz.backend import FORMATS
 
 # colorlover.scales["12"]["qual"]["Paired"] converted to hex strings
-COLORS = ["#1f78b4", "#33a02c", "#e31a1c", "#ff7f00", "#6a3d9a", "#b15928", "#a6cee3", "#b2df8a", "#fdbf6f",
-          "#cab2d6", "#ffff99"]
+
+nets_blacklist = os.environ["NETS_BLACKLIST"].split(",")
+containers_blacklist = os.environ["CONTAINERS_BLACKLIST"].split(",")
+
+nets_blacklist_dict = { nets_blacklist[i]: i for i in range(0, len(nets_blacklist)) }
+containers_blacklist_dict = { containers_blacklist[i]: i for i in range(0, len(containers_blacklist)) }
+
+DISTINCT_COLORMAP = {
+    "dimgray": "#696969",
+    "saddlebrown": "#8b4513",
+    "darkgreen": "#006400",
+    "darkmagenta": "#8b008b",
+    "red": "#ff0000",
+    "darkorange": "#ff8c00",
+    "yellow": "#ffff00",
+    "chartreuse": "#7fff00",
+    "royalblue": "#4169e1",
+    "aqua": "#00ffff",
+    "deepskyblue": "#00bfff",
+    "blue": "#0000ff",
+    "fuchsia": "#ff00ff",
+    "plum": "#dda0dd",
+    "lightgreen": "#90ee90",
+    "deeppink": "#ff1493",
+    "navajowhite": "#ffdead"
+}
+
+FONT_SIZE="10"
+EDGE_WIDTH = "2"
+EDGE_MIN_LEN = "8"
+
+BG_COLOR = DISTINCT_COLORMAP["dimgray"]
+CONTAINER_COLOR = DISTINCT_COLORMAP["navajowhite"]
+COLORS = [
+    DISTINCT_COLORMAP["saddlebrown"], 
+    DISTINCT_COLORMAP["darkgreen"], 
+    DISTINCT_COLORMAP["darkmagenta"], 
+    DISTINCT_COLORMAP["red"], 
+    DISTINCT_COLORMAP["darkorange"], 
+    DISTINCT_COLORMAP["yellow"], 
+    DISTINCT_COLORMAP["chartreuse"], 
+    DISTINCT_COLORMAP["aqua"], 
+    DISTINCT_COLORMAP["deepskyblue"], 
+    DISTINCT_COLORMAP["blue"], 
+    DISTINCT_COLORMAP["plum"], 
+    DISTINCT_COLORMAP["lightgreen"], 
+    DISTINCT_COLORMAP["deeppink"], 
+    DISTINCT_COLORMAP["fuchsia"], 
+    DISTINCT_COLORMAP["royalblue"], 
+    DISTINCT_COLORMAP["navajowhite"]
+]
 i = 0
 
 
@@ -73,6 +122,10 @@ def get_networks(client: docker.DockerClient, verbose: bool) -> typing.Dict[str,
             except (KeyError, IndexError):
                 # This network doesn't seem to be used, skip it
                 continue
+        
+        if net.name in nets_blacklist_dict:
+            # This network has been blacklisted
+            continue
 
         internal = False
         try:
@@ -102,6 +155,11 @@ def get_containers(client: docker.DockerClient, verbose: bool) -> (typing.List[C
     links: typing.List[Link] = []
 
     for container in client.containers.list():
+        
+        if container.name in containers_blacklist_dict:
+            # This container has been blacklisted
+            continue
+
         interfaces: typing.List[Interface] = []
 
         # Iterate over container interfaces
@@ -128,8 +186,10 @@ def draw_network(g: Graph, net: Network):
     label += "}"
 
     g.node(f"network_{net.name}",
-           shape="record",
+           shape="Mrecord",
            label=label,
+           fontsize=FONT_SIZE,
+           margin="0.20,0.05",
            fillcolor=net.color,
            style="filled"
            )
@@ -143,7 +203,9 @@ def draw_container(g: Graph, c: Container):
     g.node(f"container_{c.container_id}",
            shape="record",
            label=label,
-           fillcolor="#ff9999",
+           fontsize=FONT_SIZE,
+           margin="0.20,0.05",
+           fillcolor=CONTAINER_COLOR,
            style="filled"
            )
 
@@ -151,6 +213,8 @@ def draw_container(g: Graph, c: Container):
 def draw_link(g: Graph, networks: typing.Dict[str, Network], link: Link):
     g.edge(f"container_{link.container_id}:{link.endpoint_id}",
            f"network_{link.network_name}",
+           penwidth=EDGE_WIDTH,
+           minlen=EDGE_MIN_LEN,
            color=networks[link.network_name].color
            )
 
@@ -163,9 +227,9 @@ def generate_graph(verbose: bool, file: str):
 
     if file:
         base, ext = os.path.splitext(file)
-        g = Graph(comment="Docker Network Graph", engine="sfdp", format=ext[1:], graph_attr=dict(splines="true"))
+        g = Graph(comment="Docker Network Graph", engine="sfdp", format=ext[1:], graph_attr=dict(splines="true",bgcolor=BG_COLOR,rankdir="LR"))
     else:
-        g = Graph(comment="Docker Network Graph", engine="sfdp", graph_attr=dict(splines="true"))
+        g = Graph(comment="Docker Network Graph", engine="sfdp", graph_attr=dict(splines="true",bgcolor=BG_COLOR,rankdir="LR"))
 
     for _, network in networks.items():
         draw_network(g, network)
